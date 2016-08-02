de0bc0a03a4124bec73c47f03781458c925543e0
Richard Vong <richvong@outlook.com>
2016-08-01 17:01:25 -0700

    first commit after .gitignore and Gemfile update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hartlfirstapp::Application.config.secret_key_base = 'e30a478b26f962f9c4ebdefd55e803b03078907bbff18427b792be1ddda9d93ebf87cb5dca6aa77d4e05542676016e6d3bfe556f13f657f090359c90d39a9ddc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hartlfirstapp::Application.config.session_store :cookie_store, key: '_hartlfirstapp_session'
